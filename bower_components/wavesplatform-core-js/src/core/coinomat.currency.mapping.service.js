38c4b4bfa8ab398e8c86940b33ea28bbe7d85315
arsen <arsen3d@gmail.com>
2017-10-18 21:23:23 -0700

    fix screen cut-off on large monitors.

---
+++ bower_components/wavesplatform-core-js/src/core/coinomat.currency.mapping.service.js
@@ -0,0 +1,51 @@
+(function () {
+    'use strict';
+
+    angular
+        .module('waves.core.services')
+        .service('coinomatCurrencyMappingService', [function () {
+            function unsupportedCurrency(currency) {
+                throw new Error('Unsupported currency: ' + currency.displayName);
+            }
+
+            /**
+             * Currency codes for Waves Platform
+             * @param {Currency} currency
+             * @returns {string} currency code
+             */
+            this.platformCurrencyCode = function (currency) {
+                switch (currency.id) {
+                    case Currency.BTC.id:
+                        return 'WBTC';
+
+                    case Currency.WAVES.id:
+                        return 'WAVES';
+
+                    case Currency.ETH.id:
+                        return 'WETH';
+                }
+
+                unsupportedCurrency(currency);
+            };
+
+            /**
+             * Currency codes for Coinomat gateway
+             * @param {Currency} currency
+             * @returns {string} currency code
+             */
+            this.gatewayCurrencyCode = function (currency) {
+                switch (currency.id) {
+                    case Currency.BTC.id:
+                        return 'BTC';
+
+                    case Currency.WAVES.id:
+                        return 'WAVES';
+
+                    case Currency.ETH.id:
+                        return 'ETH';
+                }
+
+                unsupportedCurrency(currency);
+            };
+        }]);
+})();
