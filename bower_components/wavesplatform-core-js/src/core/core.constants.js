0a61f5a6347bb51c509a4073a712890cbf7be81d
arsen <arsen3d@gmail.com>
2017-10-18 21:23:23 -0700

    fix screen cut-off on large monitors.

---
+++ bower_components/wavesplatform-core-js/src/core/core.constants.js
@@ -0,0 +1,50 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('waves.core.constants', [])
+        .constant('constants.network', {
+            NETWORK_NAME: 'devel', // 'devnet', 'testnet', 'mainnet'
+            ADDRESS_VERSION: 1,
+            NETWORK_CODE: 'T',
+            INITIAL_NONCE: 0
+        });
+
+    angular
+        .module('waves.core.constants')
+        .constant('constants.address', {
+            RAW_ADDRESS_LENGTH : 35,
+            ADDRESS_PREFIX: '1W',
+            MAINNET_ADDRESS_REGEXP: /^[a-zA-Z0-9]{35}$/
+        });
+
+    angular
+        .module('waves.core.constants')
+        .constant('constants.features', {
+            ALIAS_VERSION: 2
+        });
+
+    angular
+        .module('waves.core.constants')
+        .constant('constants.ui', {
+            MINIMUM_PAYMENT_AMOUNT : 1e-8,
+            MINIMUM_TRANSACTION_FEE : 0.001,
+            AMOUNT_DECIMAL_PLACES : 8,
+            JAVA_MAX_LONG: 9223372036854775807,
+            MAXIMUM_ATTACHMENT_BYTE_SIZE: 140
+        });
+
+    angular
+        .module('waves.core.constants')
+        .constant('constants.transactions', {
+            PAYMENT_TRANSACTION_TYPE : 2,
+            ASSET_ISSUE_TRANSACTION_TYPE: 3,
+            ASSET_TRANSFER_TRANSACTION_TYPE: 4,
+            ASSET_REISSUE_TRANSACTION_TYPE: 5,
+            EXCHANGE_TRANSACTION_TYPE: 7,
+            START_LEASING_TRANSACTION_TYPE: 8,
+            CANCEL_LEASING_TRANSACTION_TYPE: 9,
+            CREATE_ALIAS_TRANSACTION_TYPE: 10,
+            MAKE_ASSET_NAME_UNIQUE_TRANSACTION_TYPE: 11
+        });
+})();
